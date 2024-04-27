ee3f688c8d58c7f8fec534a3f56217869966b022
ppirog <pawelpirog99@gmail.com>
2024-04-27 23:04:51 +0200

    first commit

---
+++ src/main/java/org/fasttrack/FastTrackApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fasttrack/FastTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
