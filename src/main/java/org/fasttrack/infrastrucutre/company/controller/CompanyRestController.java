4f74ae5d8fcd2ce8d196329d57f76b00b182875a
ppirog <pawelpirog99@gmail.com>
2024-05-19 21:58:41 +0200

    1) added @Valid in CompanyRestController 2) added @NotBlank and @NotNull in CompanyRequestDto

--- src/main/java/org/fasttrack/infrastrucutre/company/controller/CompanyRestController.java
+++ src/main/java/org/fasttrack/infrastrucutre/company/controller/CompanyRestController.java
@@ -1,9 +1,11 @@
 package org.fasttrack.infrastrucutre.company.controller;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.fasttrack.domain.company.CompanyFacade;
 import org.fasttrack.domain.company.dto.CompanyRequestDto;
 import org.fasttrack.domain.company.dto.CompanyResponseDto;
+import org.hibernate.validator.internal.constraintvalidators.bv.PatternValidator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/company" )
 public class CompanyRestController {
     private final CompanyFacade companyFacade;
+    private final PatternValidator patternValidator;
 
     @GetMapping("/{krs}")
     public ResponseEntity<CompanyResponseDto> findOfferById(@PathVariable String krs) {
@@ -24,7 +27,7 @@ public class CompanyRestController {
     }
 
     @PostMapping
-    public ResponseEntity<CompanyResponseDto> findOfferByName(@RequestBody CompanyRequestDto dto) {
+    public ResponseEntity<CompanyResponseDto> findOfferByName(@RequestBody @Valid CompanyRequestDto dto) {
         return ResponseEntity.ok(companyFacade.saveCompany(dto));
     }
 
